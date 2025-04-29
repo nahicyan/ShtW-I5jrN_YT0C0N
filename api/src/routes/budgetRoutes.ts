44cf92d67d598c2d90838d45245f0c44cfc6a891
Cyan <nahicyan@protonmail.com>
2025-04-29 04:44:23 -0500

    Fix-Dash-Chart-v0.0.0.3

--- api/src/routes/budgetRoutes.ts
+++ api/src/routes/budgetRoutes.ts
@@ -12,10 +12,13 @@ import {
 const router = express.Router();
 
 // Budget routes
-router.route('/summary').get(getBudgetSummary);
-router.route('/').get(getBudgetEntries).post(createBudgetEntry);
-router.route('/:id').get(getBudgetEntry).put(updateBudgetEntry).delete(deleteBudgetEntry);
-router.route('/dashboard/weekly').get(getDashboardWeekly);
+router.get('/dashboard/weekly', getDashboardWeekly);
+router.get('/summary', getBudgetSummary);
+router.get('/', getBudgetEntries);
+router.post('/', createBudgetEntry);
+router.get('/:id', getBudgetEntry);
+router.put('/:id', updateBudgetEntry);
+router.delete('/:id', deleteBudgetEntry);
 
 
 export default router;
